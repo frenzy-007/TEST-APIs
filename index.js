f54f3e465f05527642f38a9d17774ad4b793fde5
Menghour <menghourpech007@gmail.com>
2026-07-18 18:39:27 +0200

    commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+require("dotenv").config();
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
