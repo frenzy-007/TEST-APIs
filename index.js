b2106810548d48f8cdf807abf1b182f94b7d70d5
Menghour <menghourpech007@gmail.com>
2026-07-18 17:47:15 +0200

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/01", (req, res) => {
+  res.send({
+    name: "Menghour",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listen on http://localhost:${PORT}`);
+});
